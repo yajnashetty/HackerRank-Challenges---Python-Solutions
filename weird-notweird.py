ceb5f841b06d75fb180e056655aafdee092f93bf
yajnashetty <134050036+yajnashetty@users.noreply.github.com>
2025-03-20 14:53:32 +0530

    Create weird-notweird.py
    Given an integer, n, perform the following conditional actions:
    
    If n is odd, print Weird
    If n is even and in the inclusive range of 2 to 5, print Not Weird
    If n is even and in the inclusive range of 6 to 20, print Weird
    If n is even and greater than 20, print Not Weird

---
+++ weird-notweird.py
@@ -0,0 +1,14 @@
+
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+number = print("Weird" if n%2!=0 else "Not Weird" )
