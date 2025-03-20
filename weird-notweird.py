3fcaab48881a532944c572f16c0d6b1277434d95
yajnashetty <134050036+yajnashetty@users.noreply.github.com>
2025-03-20 15:25:32 +0530

    Update weird-notweird.py

--- weird-notweird.py
+++ weird-notweird.py
@@ -1,14 +1,17 @@
-
-
 import math
 import os
 import random
 import re
 import sys
 
-
-
 if __name__ == '__main__':
     n = int(input().strip())
 
-number = print("Weird" if n%2!=0 else "Not Weird" )
+    if n % 2 != 0:
+        print("Weird")
+    elif 2 <= n <= 5:
+        print("Not Weird")
+    elif 6 <= n <= 20:
+        print("Weird")
+    else:
+        print("Not Weird")
